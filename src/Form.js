3ee108cbff382ad61bb1ccaf97f44bb6ede20e1c
ta2bey371 <ta2bey371@gmeil.com>
2026-02-21 21:09:04 +0900

    曜日の自動選択と理由の選択肢を追加しました

--- src/Form.js
+++ src/Form.js
@@ -1,43 +1,79 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import curriculum from './curriculum.json'
 
 const Form = () => {
-    const [date,setDate] = useState("");
-    const [day,setDay] = useState("Mon");
-    const [classTime,setClassTime] = useState("2");
-    const [grade,setGrade] = useState("");
+    const nowDay = new Date().getFullYear()+"-"+String(new Date().getMonth()+101).slice(1)+"-"+String(new Date().getDate()+100).slice(1);
+    const dayList = [
+        ["Sun","日曜日"],
+        ["Mon","月曜日"],
+        ["Tue","火曜日"],
+        ["Wed","水曜日"],
+        ["Thu","木曜日"],
+        ["Fri","金曜日"],
+        ["Stu","土曜日"]
+    ];
+
+    const [date,setDate] = useState(nowDay);
+    const [day,setDay] = useState(["Mon","月曜日"]);
+    const [classTime,setClassTime] = useState("1");
+    const [grade,setGrade] = useState("1");
+    const [reason,setReason] = useState("病気");
 
     const changeDate = (e) => setDate(e.target.value);
-    const changeDay = (e) => setDay(e.target.value);
     const changeClassTime = (e) => setClassTime(e.target.value);
     const changeGrade = (e) => setGrade(e.target.value);
+    const changeReason = (e) => setReason(e.target.value);
+
+    useEffect(()=>{
+        const targetDate = new Date(date);
+        setDay(dayList[targetDate.getDay()]);
+    },[date])
+    useEffect(()=>{
+        console.log(reason);
+    },[reason])
   return (
-    <div>
-        <input type='date' onChange={changeDate} />
-        <select onChange={changeDay}>
-            <option value={"Mon"}>月</option>
-            <option value={"Tue"}>火</option>
-            <option value={"Wed"}>水</option>
-            <option value={"Thu"}>木</option>
-            <option value={"Fri"}>金</option>
-        </select>
-        <select onChange={changeClassTime}>
-            <option value={1}>1限</option>
-            <option value={2}>2限</option>
-            <option value={3}>3限</option>
-            <option value={4}>4限</option>
-            <option value={5}>5限</option>
-            <option value={6}>6限</option>
-        </select>
-        <select onChange={changeGrade}>
-            <option value={1}>1年次</option>
-            <option value={2}>2年次</option>
-            <option value={3}>3年次</option>
-            <option value={4}>4年次</option>
-        </select>
-        <p>{date},{day},{classTime},{grade}</p>
-        {curriculum[1][day][classTime]?<p>{curriculum[1][day][classTime]}</p>:<p></p>}
-    </div>
+    <>
+        <div>
+            <input type='date' onChange={changeDate} value={date} max={nowDay} />
+            <p>{day[1]}</p>
+            <select onChange={changeClassTime}>
+                <option value={1}>1限</option>
+                <option value={2}>2限</option>
+                <option value={3}>3限</option>
+                <option value={4}>4限</option>
+                <option value={5}>5限</option>
+                <option value={6}>6限</option>
+            </select>
+            <select onChange={changeGrade}>
+                <option value={1}>1年次</option>
+                <option value={2}>2年次</option>
+                <option value={3}>3年次</option>
+                <option value={4}>4年次</option>
+            </select>
+            {curriculum[grade][day[0]][classTime][0] !== ""?<p>{curriculum[grade][day[0]][classTime]}</p>:<p>該当する授業はありません。</p>}
+        </div>
+        <div>
+            <select onChange={changeReason}>
+                <option>病気</option>
+                <option>交通機関の遅延</option>
+                <option>忌引き</option>
+                <option>その他</option>
+            </select>
+            {reason === "交通機関の遅延" ? 
+            <>
+                <label>
+                    欠席
+                    <input type='radio' name='isAttend'/>
+                </label>
+                <label>
+                    遅刻
+                    <input type='radio' name='isAttend'/>
+                </label>
+            </>:!(reason === "病気" || reason === "忌引き") && 
+            <input type='text'/>
+            }
+        </div>
+    </>
   )
 }
 
