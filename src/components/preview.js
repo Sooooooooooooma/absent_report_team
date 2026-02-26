c817495bd6ae7efd452b3b255d8815bbc9ac32ac
ta2bey371 <ta2bey371@gmeil.com>
2026-02-26 21:24:37 +0900

    入力された情報から文章を作成できるようにしました

--- src/components/preview.js
+++ src/components/preview.js
@@ -5,10 +5,12 @@ import { useNavigate } from 'react-router-dom'
 
 function Preview({ userInf, absentForm }) {
   const navigate = useNavigate();
+  const text = `${absentForm.subject}を受講している${userInf.stName}(学籍番号：${userInf.stNum})です。\n${absentForm.date}の授業を${absentForm.reason}により${absentForm.isAttend}したため、その証明書を以下に添付します。`
   return (
     <>
       <Header />
       <div>
+        <p>{text}</p>
         <button className='submit'>送信</button>
       </div>
     </>
