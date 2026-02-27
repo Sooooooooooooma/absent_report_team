681bd8b6206e595525f280d05379893177dbeb06
安川蒼真 <yasukawaaoimakoto@yasukawaaoimakotonoMacBook-Air.local>
2026-02-27 10:41:28 +0900

    logim機能の追加

--- src/App.js
+++ src/App.js
@@ -1,19 +1,77 @@
+// import {
+//   useState
+// } from "react";
+// import Complete from "./components/Complete.js"
+// import Form from "./components/Form.js"
+// import header from "./components/header.js"
+// import Login from "./components/login.js"
+// import Preview from "./components/preview.js"
+// import Reback from "./components/rebeck.js"
+// import Myself from "./components/myself.js"
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// function App() {
+//   const [userInf, setUserInf] = useState({
+//     "stNum": "",
+//     "stName": "",
+//   })
+
+//   const catchUser=(data)=>{
+//     setUserInf(data);
+//     // console.log(data)
+//   };
+
+//   const [absentForm, setAbsentForm] = useState({
+//     "date": "",  //日にち
+//     "day": "",  //曜日
+//     "grade":"", //学年
+//     "class":"", //何限？？？？？
+//     "subject": "",//教科
+//     "reason": "",//理由
+//     "isAttend": "",//遅刻or欠席
+//     "file": "",//ファイル
+//   })
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         <Route path="/Login" element={<Login catchUser={catchUser}/>}/>
+//         <Route path="/Form" element={<Form userInf={userInf}/>}/>
+//         <Route path="/Preview" element={<Preview userInf={userInf} absentForm={absentForm}/>}/>
+//         <Route path="/Complete" element={<Complete/>}/>
+//         <Route path="/Reback" element={<Reback/>}/>
+//         <Route path="/Myself" element={<Myself userInf={userInf}/>}/>
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+
 import {
   useState
 } from "react";
 import Complete from "./components/Complete.js"
 import Form from "./components/Form.js"
-import header from "./components/header.js"
+import Header from "./components/header.js"
 import Login from "./components/login.js"
 import Preview from "./components/preview.js"
 import Reback from "./components/rebeck.js"
+import Myself from "./components/myself.js"
+import Introduce from "./components/Introduce.js";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [userInf, setUserInf] = useState({
-    "stNum": "",
-    "stName": "",
+    "stNum": "", //学籍番号
+    "stName": "", //名前
   })
+
+  //子からデータを受け取る関数
+  const catchUser=(data)=>{
+    setUserInf(data);
+    // console.log(data)
+  };
 
   const [absentForm, setAbsentForm] = useState({
     "date": "",  //日にち
@@ -24,15 +82,22 @@ function App() {
     "reason": "",//理由
     "isAttend": "",//遅刻or欠席
     "file": "",//ファイル
+
+
   })
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/Login" element={<Login/>}/>
-        <Route path="/Form" element={<Form/>}/>
-        <Route path="/Preview" element={<Preview userInf={userInf} absentForm={absentForm}/>}/>
-        <Route path="/Complete" element={<Complete/>}/>
+        <Route path="/Login" element={<Login catchUser={catchUser}/>}/>
+        <Route path="/Form" element={<Form userInf={userInf}/>}/>
+        <Route path="/Preview" element={<Preview/>}/>
+        <Route path="/Conplite" element={<Complete/>}/>
         <Route path="/Reback" element={<Reback/>}/>
+        <Route path="/Myself" element={<Myself userInf={userInf}/>}/>
+        <Route path="/Introduce" element={<Introduce/>}/>
+        
       </Routes>
     </BrowserRouter>
   );
