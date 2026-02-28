6608f3c014dbef8284689374e8bd399734e37e14
ta2bey371 <ta2bey371@gmeil.com>
2026-02-28 19:20:39 +0900

    モックデータを入力しました不要になれば削除してください

--- src/components/preview.js
+++ src/components/preview.js
@@ -5,14 +5,19 @@ import { useNavigate } from 'react-router-dom'
 
 function Preview({ userInf, absentForm }) {
   const navigate = useNavigate();
-  const text = `${absentForm.subject}を受講している${userInf.stName}(学籍番号：${userInf.stNum})です。\n${absentForm.date}の授業を${absentForm.reason}により${absentForm.isAttend}したため、その証明書を以下に添付します。`
+  const text = `${absentForm.subject}を受講している${userInf.stName}(学籍番号：${userInf.stNum})です。\n${absentForm.date}の授業を${absentForm.reason}により${absentForm.isAttend}したため、その証明書を以下に添付します。`;
+  const sendMail = () => {
+    
+    navigate("/Conplite");
+  };
   return (
     <>
       <Header />
       <div>
         <p>{text}</p>
         <img src={absentForm.file} alt='画像が読み込めません'/>
-        <button className='submit'>送信</button>
+        <button onClick={()=> navigate("/Form")}>入力画面へ戻る</button>
+        <button className='submit' onClick={sendMail}>送信</button>
       </div>
     </>
   )
